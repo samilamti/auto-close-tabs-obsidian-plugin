94423cd6cbeacf3c3fca4ed2de6e7e0630597c33
Sami Lamti <sami.lamti@gmail.com>
2026-02-15 19:11:43 +0100

    It's publishin' time!

---
+++ main.ts
@@ -0,0 +1,41 @@
+import { Plugin, WorkspaceLeaf } from 'obsidian';
+
+export default class AutoCloseTabsPlugin extends Plugin {
+    lastActiveMap: Map<WorkspaceLeaf, number> = new Map();
+
+    async onload() {
+        const updateTimestamps = (leaf: WorkspaceLeaf | null) => {
+            if (!leaf || leaf.getViewState().pinned) return;
+            this.updateTimestamp(leaf);
+        };
+
+        this.registerEvent(
+            this.app.workspace.on('active-leaf-change', updateTimestamps)
+        );
+        this.app.workspace.iterateAllLeaves(updateTimestamps);
+
+        this.registerInterval(
+            window.setInterval(() => this.checkInactiveTabs(), 60 * 1000)
+        );
+    }
+
+    updateTimestamp(leaf: WorkspaceLeaf) {
+        this.lastActiveMap.set(leaf, Date.now());
+    }
+
+    checkInactiveTabs() {
+        const fiveMinutes = 5 * 60 * 1000;
+        const now = Date.now();
+
+        this.app.workspace.iterateAllLeaves((leaf) => {
+            const lastActive = this.lastActiveMap.get(leaf);
+
+            if (leaf.getViewState().pinned) return;
+
+            if (lastActive && (now - lastActive > fiveMinutes)) {
+                leaf.detach();
+                this.lastActiveMap.delete(leaf);
+            }
+        });
+    }
+}
